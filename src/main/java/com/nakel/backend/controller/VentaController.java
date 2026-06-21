e5f795f611363697f3088be8c7931c69ecac7487
Lionel Lara <lionellara2002@gmail.com>
2026-06-21 19:12:07 -0300

    back estable primer parte de mostrador / catalogo

--- src/main/java/com/nakel/backend/controller/VentaController.java
+++ src/main/java/com/nakel/backend/controller/VentaController.java
@@ -3,6 +3,8 @@ package com.nakel.backend.controller;
 import com.nakel.backend.model.Venta;
 import com.nakel.backend.service.VentaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +17,11 @@ public class VentaController {
     private VentaService ventaService;
 
     @PostMapping
-    public Venta cobrarVenta(@RequestBody Venta venta) {
-        // El mozo le pasa el pedido a la cocina y devuelve el plato listo
-        return ventaService.procesarYGuardarVenta(venta);
+    public ResponseEntity<Venta> cobrarVenta(@RequestBody Venta venta) {
+        // 1. Procesamos la venta en el service
+        Venta ventaGuardada = ventaService.procesarYGuardarVenta(venta);
+
+        // 2. Retornamos 201 Created, que es lo que tu Frontend está esperando
+        return ResponseEntity.status(HttpStatus.CREATED).body(ventaGuardada);
     }
 }
